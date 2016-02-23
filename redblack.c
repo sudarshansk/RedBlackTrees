6095b362d643b2476c58bb11085c2453a56a031d
akash-suresh <akashsuresh36@gmail.com>
2016-02-23 16:21:41 +0530

    Updated insertion - 2

--- redblack.c
+++ redblack.c
@@ -24,30 +24,60 @@ node* new_node(int val){
 	new1->color=0;
 	return new1;
 }
+
+node* rearrange(node *a, node *b, node *c, node *d){
+	b->lptr = a;
+	a->parent = b;
+
+	b->rptr = c;
+	c->parent = b;
+
+	b->color = 1;
+	a->color = 0;
+	c->color = 0;
+
+	if(d!=NULL){
+		if(d->val>b->val){
+			c->lptr = d;
+			d->parent = c;
+		}
+		else{
+			a->rptr = d;
+			d->parent = a;
+		}
+	}
+	return b;
+}
 void insert_node(node *root, int val){
 	
-	node *temp=root;
-
+	node *temp=root,prev,grand,uncle,great_grand,a,b,c,new1;
+	int dir,dir2;
+	new1 = new_node(val);
 	if (root==NULL){
-		root = new_node(val);
+		root = new1;
 		root->color=1;
 		return;
 	}
 	while(temp!=NULL){
-		if(temp->data < val){
+		if(val > temp->data){
 			prev=temp;
 			temp = temp->rptr;
+			dir2=1;
 		}
 		else{
 			prev=temp;
 			temp = temp->lptr;
+			dir2=0;
 		}
 	}
 	if(prev->color == 1){
-		temp = new_node(val);
+		temp = new1;
+		new1->parent = prev;
+		return;
 	}
 	else{
 		grand = prev->parent;
+		great_grand=grand->parent;
 		if(grand->lptr==prev){
 			uncle = grand->rptr;	
 			dir = 0;
@@ -56,40 +86,61 @@ void insert_node(node *root, int val){
 			uncle = grand->lptr;
 			dir = 1;
 		}
+
 		if(uncle->color==1){
-			//uncle is red
+			//uncle is black
 			if(dir==0){
-				if(val<prev->data){
-					
+				if(dir2==1){
+					a = prev;
+					b = new1;
+					c = grand;
+					grand = rearrange(a,b,c,NULL);
+					grand->parent = great_grand;
 				}
 				else{
-
+					a = new1;
+					b = prev;
+					c = grand;
+					d = prev->rptr;
+					grand = rearrange(a,b,c,d);
+					grand->parent = great_grand;
 				}
 			}
-			if(dir==1){
-				if(val<prev->data){
-
+			else{
+				if(dir2==0){
+					a = grand;
+					b = new1;
+					c = prev;
+					grand = rearrange(a,b,c,NULL);
+					grand->parent = great_grand;
 				}
 				else{
-
+					a = grand;
+					b = prev;
+					c = new1;
+					d = prev->lptr;
+					grand = rearrange(a,b,c,d);
+					grand->parent = great_grand;
 				}
 			}
+			return;
 		}
 		else{
 			//uncle is also red
-			if(prev->data<val){
-				prev->lptr=new_node(val);
+			if(dir2==0){
+				prev->lptr=new1;
 			}
 			else{
-				prev->rptr=new_node(val);
+				prev->rptr=new1;
 			}
-			grand = prev->parent;
+			new1->parent = prev;
+
 			(grand->rptr)->color=1;
 			(grand->lptr)->color=1;
-			if(grand->color == 1){
-				grand->color=0;
-				
-			}
+			
+			grand->color = 0;
+			
+			
 		}
 	}
 }
